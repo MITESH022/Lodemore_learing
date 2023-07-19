49d100006960295efd4be1c8d10e9acd0ba46338
mitesh] <miteshap94@gamil.com>
2023-07-19 17:36:21 +0530

    UI ready data is show

--- my-app/src/Pages/Api.jsx
+++ my-app/src/Pages/Api.jsx
@@ -1,13 +1,54 @@
-import React from 'react'
-import Header from '../component/Header'
+import React, { useEffect, useState } from 'react';
+import Header from '../component/Header';
 
 const Api = () => {
-  return (
-   <>
-   <Header/>
-   API page
-   </>
-  )
-}
+  const [Product, setProduct] = useState([]);
+  const [visibleProduct, setVisibleProduct] = useState([]);
 
-export default Api
+  const URL = `https://dummyjson.com/products`;
+
+  useEffect(() => {
+    getProduct();
+  }, []);
+
+  const getProduct = async () => {
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      setProduct(data.products);
+    } catch (error) {
+      console.log(error, "producterror");
+    }
+  };
+
+  return (
+    <>
+      <Header />
+      <h1 className='text-center'> API with Loading more </h1>
+      <div className='container'>
+        <div className='row gy-3 my-3'>
+          { Product.slice(0,4).map((item, index) => (
+              <div className='col-md-3 col-sm-6' key={index}>
+                <div className='card' style={{ width: "18rem",height:"30rem" }}>
+                  <img src={item.thumbnail} className='card-img-top' alt='no product' />
+                  <div className='card-body'>
+                    <h5 className='card-title'>{item.title}</h5>
+                    <p className='card-text'>
+                     {item.description}
+                    </p>
+                    <a href='#' className='btn btn-primary'>
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
+              </div>
+            ))}
+            <button className='btn btn-primary'>LOAD MORE</button>
+
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Api;
---
+++ my-app/src/component/Card.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+   <>
+      {/* <div className='row gy-3 my-3'>
+                <div className='col-md-3 col-sm-6'>
+                  <div class="card" style={{width: "18rem"}}>
+             <img src="" class="card-img-top" alt="no producat"/>
+             <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+         </div>
+        </div>
+                </div>
+                <div className='col-md-3  col-sm-6'>
+                  <div class="card" style={{width: "18rem"}}>
+             <img src="" class="card-img-top" alt="no producat"/>
+             <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+         </div>
+        </div>
+                </div>
+                <div className='col-md-3  col-sm-6'>
+                  <div class="card" style={{width: "18rem"}}>
+             <img src="" class="card-img-top" alt="no producat"/>
+             <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+         </div>
+        </div>
+                </div>
+                <div className='col-md-3  col-sm-6'>
+                  <div class="card" style={{width: "18rem"}}>
+             <img src="" class="card-img-top" alt="no producat"/>
+             <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+         </div>
+        </div>
+        
+                </div>
+            </div>  */}
+   </>
+  )
+}
+
+export default Card
